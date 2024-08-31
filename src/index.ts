305a2eb79d7fbf92d7d0e42e13586b0333c92712
tshinohara <takuma1201@gmail.com>
2024-08-31 17:05:17 +0900

    不要なclose削除

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,6 @@ const RELAYS = [
 ];
 
 const pool = new SimplePool();
-const close = () => {
-  pool.close(RELAYS);
-};
 
 const send = async (content: string, targetEvent: Event | null = null) => {
   const created = targetEvent ? targetEvent.created_at + 1 : currUnixtime();
